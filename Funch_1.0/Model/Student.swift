039cba7453798c48a3b9ea3faff8abe0f90dcfb5
Guillermo Ramos <memo33101@gmail.com>
2019-10-05 20:19:33 -0600

    Created Student object

---
+++ Funch_1.0/Model/Student.swift
@@ -0,0 +1,15 @@
+//
+//  Student.swift
+//  Funch_1.0
+//
+//  Created by Guillermo Ramos  on 10/5/19.
+//  Copyright © 2019 Guillermo Ramos . All rights reserved.
+//
+
+import UIKit
+
+struct Student {
+    var image: UIImage
+    var title: String
+    
+}
