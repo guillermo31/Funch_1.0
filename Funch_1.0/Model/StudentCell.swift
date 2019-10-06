c7a145ccdcd8731e8a494c229649025342e509fc
Guillermo Ramos <memo33101@gmail.com>
2019-10-05 19:52:50 -0600

    Created Custom ViewCell file

---
+++ Funch_1.0/Model/StudentCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentCell.swift
+//  Funch_1.0
+//
+//  Created by Guillermo Ramos  on 10/5/19.
+//  Copyright © 2019 Guillermo Ramos . All rights reserved.
+//
+
+import UIKit
+
+class StudentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
